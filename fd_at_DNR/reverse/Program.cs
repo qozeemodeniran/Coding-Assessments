04973dd7fae36b723c67ae5f66d44ebd288d2475
Qozeem Odeniran <muhdqozeem@gmail.com>
2024-04-25 06:10:09 -0400

    solved reversing a string without using inbuilt method

---
+++ fd_at_DNR/reverse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class ReverseString{
+    static string Reverse(string str){
+        char[] charArray = str.ToCharArray();
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right){
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new string(charArray);
+    }
+
+    static void Main(string[] args){
+        string input = "Qozeem";
+        string reversed = Reverse(input);
+        Console.WriteLine($"The reversed of {input} is {reversed}");
+    }
+}
